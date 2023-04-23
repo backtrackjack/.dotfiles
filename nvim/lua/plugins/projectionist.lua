b4200024197d8b8752332c5efaf25d48ed758643
Jack Sellars <jsellars@enee.com.au>
2023-04-23 21:16:09 +0930

    feat(projectionist): add projectionist with laravel config

---
+++ nvim/lua/plugins/projectionist.lua
@@ -0,0 +1,41 @@
+return {
+    'tpope/vim-projectionist',
+    dependencies = { 'tpope/vim-dispatch' },
+    config = function()
+        vim.g.projectionist_heuristics = {
+            artisan = {
+                ['*'] = {
+                    start = 'php artisan serve',
+                    console = 'php artisan tinker'
+                },
+                ['app/*.php'] = {
+                    type = 'source',
+                    alternate = {
+                        'tests/Unit/{}Test.php',
+                        'tests/Feature/{}Test.php'
+                    }
+                },
+                ['tests/Feature/*Test.php'] = {
+                    type = 'test',
+                    alternate = 'app/{}.php'
+                },
+                ['tests/Unit/*Test.php'] = {
+                    type = 'test',
+                    alternate = 'app/{}.php'
+                },
+                ['app/Models/*.php'] = {
+                    type = 'model',
+                },
+                ['app/Http/Controllers/*.php'] = {
+                    type = 'controller'
+                },
+                ['routes/*.php'] = {
+                    type = 'route',
+                },
+                ['database/migrations/*.php'] = {
+                    type = 'migration',
+                }
+            }
+        }
+    end
+}
