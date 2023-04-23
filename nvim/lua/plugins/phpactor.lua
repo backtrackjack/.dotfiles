a661bccfe8613d69482ee8478275bfc573f8163f
Jack Sellars <jsellars@enee.com.au>
2023-04-23 21:17:00 +0930

    fix(phpactor): adjust keymap

--- nvim/lua/plugins/phpactor.lua
+++ nvim/lua/plugins/phpactor.lua
@@ -3,6 +3,6 @@ return {
     ft = 'php',
     build = 'composer install --no-dev --optimize-autoloader',
     config = function()
-        vim.keymap.set('n', '<leader>pm', ':PhpactorMoveFile<cr>')
+        vim.keymap.set('n', '<leader>pfm', ':PhpactorMoveFile<cr>')
     end
 }
