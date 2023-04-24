d03a072214d18d8d9694c2cd0dfd536df41170a1
Jack Sellars <jsellars@enee.com.au>
2023-04-24 11:53:22 +0930

    feat(bufferline): highlights, custom area and style

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -27,17 +27,68 @@ return {
                     text_align = "left",
                 },
             },
-            separator_style = "thin",
+            separator_style = "slope",
+            custom_areas = {
+                left = function()
+                    return {
+                        { text = '    ', fg = '#8fff6d' },
+                    }
+                end
+            }
         },
         highlights = {
             fill = {
-                bg = "#1b1b1b"
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            background = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            tab = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            tab_close = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            close_button = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+                fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+            },
+            close_button_visible = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+                fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+            },
+            close_button_selected = {
+                fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+            },
+            buffer_visible = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            modified = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            modified_visible = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            duplicate = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            duplicate_visible = {
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            separator = {
+                fg = { attribute = 'bg', highlight = 'StatusLine' },
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
+            },
+            separator_selected = {
+                fg = { attribute = 'bg', highlight = 'StatusLine' },
+                bg = { attribute = 'bg', highlight = 'Normal' }
+            },
+            separator_visible = {
+                fg = { attribute = 'bg', highlight = 'StatusLine' },
+                bg = { attribute = 'bg', highlight = 'StatusLine' },
             },
             indicator_selected = {
-                fg = {
-                    attribute = "fg",
-                    highlight = "ErrorMsg",
-                },
+                fg = { attribute = "fg", highlight = "ErrorMsg" },
             },
         },
     },
