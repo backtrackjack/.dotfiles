183f6266c6f423232eef18dcfa424cf5ffa50fbb
Jack Sellars <jsellars@enee.com.au>
2023-05-09 02:11:59 +0930

    feat: move to lsp-zero config

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,54 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer',
+    'intelephense',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.configure('intelephense', {
+    filetypes = { "php" },
+    ignore_filetypes = { "blade" }
+})
+
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.config.disable,
+    ['<S-Tab>'] = cmp.config.disable,
+    -- copilot formatting is weird if you don't use enter to confirm
+    -- ['<CR>'] = cmp.config.disable,
+})
+
+lsp.setup_nvim_cmp({
+    preselect = cmp.PreselectMode.None,
+    mapping = cmp_mappings,
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+})
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
