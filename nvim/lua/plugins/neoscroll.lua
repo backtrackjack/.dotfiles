42305f70ff1f41e592f675d33edf206086c0a842
Jack Sellars <jsellars@enee.com.au>
2023-05-08 11:30:13 +0930

    fix: disable conflicting keymaps

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -1,19 +1,21 @@
 return {
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup()
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require("neoscroll").setup({
+                mappings = { '<C-u>', '<C-d>', 'zz', 'zt', 'zb' }
+            })
 
-      require("neoscroll.config").set_mappings({
-        ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "50" } },
-        ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "50" } },
-      })
-    end,
-    enabled = function()
-      if vim.g.neovide then
-        return false
-      end
-      return true
-    end,
-  },
+            require("neoscroll.config").set_mappings({
+                ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "50" } },
+                ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "50" } },
+            })
+        end,
+        enabled = function()
+            if vim.g.neovide then
+                return false
+            end
+            return true
+        end,
+    },
 }
