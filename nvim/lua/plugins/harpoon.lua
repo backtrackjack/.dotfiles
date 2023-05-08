7a4b063f573b133dd1e513b631fc0d469f11b131
Jack Sellars <jsellars@enee.com.au>
2023-05-08 13:01:00 +0930

    fix: need leader prefix

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -8,9 +8,9 @@ return {
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
         vim.keymap.set("n", "<C-t>", function() ui.nav_prev() end)
         vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
-        vim.keymap.set("n", "1", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "2", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "3", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "4", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
     end
 }
