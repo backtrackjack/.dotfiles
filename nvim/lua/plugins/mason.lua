3a66336397949ea9dbf8f8410d1cb61447800f07
Jack Sellars <jsellars@enee.com.au>
2023-05-08 11:30:51 +0930

    refactor

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,15 +1,16 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "prettierd",
-      "pint",
-      "stylua",
-      "flake8",
-      "shfmt",
-      "rust-analyzer",
-      "rustfmt",
-      "blade-formatter",
+    "williamboman/mason.nvim",
+    opts = {
+        ensure_installed = {
+            "prettierd",
+            "pint",
+            "stylua",
+            "flake8",
+            "shfmt",
+            "rust-analyzer",
+            "rustfmt",
+            "blade-formatter",
+        },
+        automatic_installation = false,
     }
-  }
 }
--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,19 +1,31 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function()
-    local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        nls.builtins.formatting.pint,
-        nls.builtins.formatting.prettierd,
-        nls.builtins.formatting.rustfmt,
-        nls.builtins.formatting.blade_formatter,
-        -- this takes some setup
-        -- nls.builtins.diagnostics.erb_lint,
-        nls.builtins.diagnostics.rubocop,
-        nls.builtins.diagnostics.php
-      }
-    }
-  end
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+        local nls = require("null-ls")
+        return {
+            root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+            sources = {
+                nls.builtins.code_actions.proselint.with({
+                    extra_filetypes = { "norg" },
+                }),
+                nls.builtins.formatting.pint,
+                nls.builtins.formatting.prettierd,
+                nls.builtins.formatting.rustfmt,
+                nls.builtins.formatting.blade_formatter,
+                -- this takes some setup
+                -- nls.builtins.diagnostics.erb_lint,
+                nls.builtins.diagnostics.rubocop,
+                nls.builtins.diagnostics.php,
+                nls.builtins.diagnostics.alex.with({
+                    extra_filetypes = { "norg" },
+                }),
+                nls.builtins.diagnostics.write_good.with({
+                    extra_filetypes = { "norg" },
+                }),
+                nls.builtins.diagnostics.proselint.with({
+                    extra_filetypes = { "norg" },
+                }),
+            }
+        }
+    end
 }
