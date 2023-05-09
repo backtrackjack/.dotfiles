3a400cf191fe42245954dfa5ef4fd2a5c52a6bf4
Jack Sellars <jsellars@enee.com.au>
2023-05-09 12:16:16 +0930

    refactor(nvim): we do a little refactoring

--- nvim/lua/plugins/zen.lua
+++ nvim/lua/plugins/zen.lua
@@ -1,36 +1,40 @@
 return {
     "folke/zen-mode.nvim",
     dependencies = "folke/twilight.nvim",
-    config = function()
-        vim.keymap.set("n", "<leader>zz", function()
-            require("twilight").setup {
-                context = 10,
-                treesitter = true,
-            }
-            require("zen-mode").setup {
-                window = {
-                    backdrop = 1,
-                    height = 0.9,
-                    width = 140,
-                    options = {
-                        number = false,
-                        relativenumber = false,
-                        signcolumn = "no",
-                        colorcolumn = "",
-                        list = false,
-                        cursorline = false,
-                    }
-                },
-                plugins = {
-                    twilight = { enabled = true },
-                    tmux = { enabled = true },
-                    alacritty = {
-                        enabled = true,
-                        font = "20"
+    keys = {
+        {
+            "<leader>zz",
+            function()
+                require("twilight").setup {
+                    context = 10,
+                    treesitter = true,
+                }
+                require("zen-mode").setup {
+                    window = {
+                        backdrop = 1,
+                        height = 0.9,
+                        width = 140,
+                        options = {
+                            number = false,
+                            relativenumber = false,
+                            signcolumn = "no",
+                            colorcolumn = "",
+                            list = false,
+                            cursorline = false,
+                        }
+                    },
+                    plugins = {
+                        twilight = { enabled = true },
+                        tmux = { enabled = true },
+                        alacritty = {
+                            enabled = true,
+                            font = "20"
+                        }
                     }
                 }
-            }
-            require("zen-mode").toggle()
-        end)
-    end
+                require("zen-mode").toggle()
+            end,
+            desc = { "zen mode" }
+        }
+    }
 }
