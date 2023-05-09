d6a6a96eebc835f74cbb81cad08ec63873771552
Jack Sellars <jsellars@enee.com.au>
2023-05-09 10:48:25 +0930

    fix(nvim): copilot formats correctly on C-y

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -30,12 +30,14 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    }),
     ["<C-Space>"] = cmp.mapping.complete(),
     ['<Tab>'] = cmp.config.disable,
     ['<S-Tab>'] = cmp.config.disable,
-    -- copilot formatting is weird if you don't use enter to confirm
-    -- ['<CR>'] = cmp.config.disable,
+    ['<CR>'] = cmp.config.disable,
 })
 
 lsp.setup_nvim_cmp({
