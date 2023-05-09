08d4ac5e27b6859c7b543f0b1777817ea0d510f1
Jack Sellars <jsellars@enee.com.au>
2023-05-09 10:48:46 +0930

    feat(nvim): add cmp emoji

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
             { 'hrsh7th/nvim-cmp', },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-emoji' },
             { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-nvim-lua' },
