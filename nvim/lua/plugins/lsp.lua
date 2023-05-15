67d7fce748bb8738e60baa8f63c497ad0b33c790
Jack Sellars <jsellars@enee.com.au>
2023-05-15 10:26:22 +0930

    refactor(nvim): move lsp related stuff to lazy folder

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -33,7 +33,12 @@ return {
                 "neovim/nvim-lspconfig",
                 opts = { autoformat = false }
             },
-            { "williamboman/mason.nvim", },
+            {
+                "williamboman/mason.nvim",
+                opts = {
+                    ui = { border = 'rounded' }
+                }
+            },
             { 'williamboman/mason-lspconfig.nvim' },
 
             --cmp
@@ -47,7 +52,68 @@ return {
             --snip
             { 'L3MON4D3/LuaSnip' },
             { 'rafamadriz/friendly-snippets' }
-        }
+        },
+        config = function()
+            local lsp = require("lsp-zero")
+
+            lsp.preset("recommended")
+
+            lsp.ensure_installed({
+                'tsserver',
+                'rust_analyzer',
+                'intelephense',
+            })
+
+            -- Fix Undefined global 'vim'
+            lsp.configure('lua_ls', {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' }
+                        }
+                    }
+                }
+            })
+
+            lsp.configure('intelephense', {
+                filetypes = { "php" },
+                ignore_filetypes = { "blade" }
+            })
+
+            lsp.configure('solargraph', {
+                cmd = { "solargraph", "stdio" },
+            })
+
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local cmp_mappings = lsp.defaults.cmp_mappings({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
+                }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<Tab>'] = cmp.config.disable,
+                ['<S-Tab>'] = cmp.config.disable,
+                ['<CR>'] = cmp.config.disable,
+            })
+
+            lsp.setup_nvim_cmp({
+                preselect = cmp.PreselectMode.None,
+                mapping = cmp_mappings,
+            })
+
+            lsp.set_preferences({
+                suggest_lsp_servers = false,
+            })
+
+            lsp.setup()
+
+            vim.diagnostic.config({
+                virtual_text = false
+            })
+        end
     },
     {
         "hrsh7th/nvim-cmp",
@@ -57,6 +123,11 @@ return {
             local cmp = require("cmp")
             opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
         end
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = true
     }
-
 }
