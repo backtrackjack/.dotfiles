effb5c3d9a7d3e1d5afef826e2cc4c48034e4e1b
Jack Sellars <jsellars@enee.com.au>
2023-05-15 10:27:43 +0930

    feat(nvim): refactoring plugin

---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,19 @@
+return {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+        {"nvim-lua/plenary.nvim"},
+        {"nvim-treesitter/nvim-treesitter"}
+    },
+    config = function()
+        -- load refactoring Telescope extension
+        require("telescope").load_extension("refactoring")
+
+        -- remap to open the Telescope refactoring menu in visual mode
+        vim.api.nvim_set_keymap(
+            "v",
+            "<leader>rr",
+            "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+            { noremap = true }
+        )
+    end
+}
