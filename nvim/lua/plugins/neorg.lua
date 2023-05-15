9ef739d107b31263939d1f043cd1cbac4d372450
Jack Sellars <jsellars@enee.com.au>
2023-05-15 10:28:01 +0930

    refactor(nvim): formatting stuff

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,43 +1,44 @@
 -- note: on macos run `cc=/usr/bin/gcc nvim -c "tsinstallsync norg"` to sync parsers
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  event = "VimEnter",
-  opts = {
-    load = {
-      ["core.defaults"] = {}, -- loads default behaviour
-      ["core.keybinds"] = {
-        config = {
-          hook = function(keybinds)
-            keybinds.unmap("norg", "n", "nt")
-          end,
-        }
-      },
-      ["core.export"] = {},
-      ["core.concealer"] = {
-        config = {
-          icon_preset = "varied",
-          dim_code_blocks = {
-            enabled = true,
-            padding = { left = 1 },
-          },
+    "nvim-neorg/neorg",
+    version = "4.0.1", -- remove when bug fixed
+    build = ":Neorg sync-parsers",
+    event = "VimEnter",
+    opts = {
+        load = {
+            ["core.defaults"] = {}, -- loads default behaviour
+            ["core.keybinds"] = {
+                config = {
+                    hook = function(keybinds)
+                        keybinds.unmap("norg", "n", "nt")
+                    end,
+                }
+            },
+            ["core.export"] = {},
+            ["core.concealer"] = {
+                config = {
+                    icon_preset = "varied",
+                    dim_code_blocks = {
+                        enabled = true,
+                        padding = { left = 1 },
+                    },
+                },
+            },
+            ["core.dirman"] = { -- manages neorg workspaces
+                config = {
+                    workspaces = {
+                        notes = "~/.dotfiles/neorg/notes",
+                        work = "~/.dotfiles/neorg/work",
+                    },
+                    default_workspace = "notes",
+                },
+            },
+            ["core.completion"] = {
+                config = {
+                    engine = "nvim-cmp",
+                },
+            },
         },
-      }, -- adds pretty icons to your documents
-      ["core.dirman"] = { -- manages neorg workspaces
-        config = {
-          workspaces = {
-            notes = "~/.dotfiles/neorg/notes",
-            work = "~/.dotfiles/neorg/work",
-          },
-          default_workspace = "notes",
-        },
-      },
-      ["core.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      },
     },
-  },
-  dependencies = { { "nvim-lua/plenary.nvim" } },
+    dependencies = { { "nvim-lua/plenary.nvim" } },
 }
