3f4af4f859cf0411425286e74c0f1b78037079b0
Jack Sellars <jsellars@enee.com.au>
2023-05-15 10:28:32 +0930

    feat(tmux): cheat sheet cmd

---
+++ tmux/tmux-cht.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+languages=$(
+    echo "php rust javascript laravel html lua ruby" | tr " " "\n"
+)
+core_utils=$(
+    echo "find sed git awk curl" | tr " " "\n"
+)
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "Input query: " query
+
+if echo "$languages" | grep -qs $selected; then
+    tmux split-window -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less -r"
+else
+    tmux split-window -h bash -c "curl cht.sh/$selected~$query | less -r"
+fi
