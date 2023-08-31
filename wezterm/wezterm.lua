8c234412a7f77ec9aeb14dcb067017f5504f2d31
Jack Sellars <jsellars@enee.com.au>
2023-08-31 11:15:33 +0930

    wezterm config and nvim updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,11 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'cyberpunk'
 
 config.enable_tab_bar = false
+
+config.font = wezterm.font_with_fallback { 'Mononoki Nerd Font Mono', 'Andale Mono' }
+config.font_size = 16
 
 return config
