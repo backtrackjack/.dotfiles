985ac38346a837ae34e0b4b7b1f27f2b199a2504
Jack Sellars <jsellars@enee.com.au>
2023-10-24 11:14:33 +1030

    keymap descriptions

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -106,7 +106,6 @@ return {
           { name = 'nvim_lsp' },
           { name = 'nvim_lsp_signature_help' },
           { name = 'luasnip' },
-          -- { name = 'copilot' },
           { name = 'buffer' },
           { name = 'path' },
         },
@@ -114,6 +113,18 @@ return {
           ghost_text = true,
         },
       }
+
+      -- forget snippet when changing mode
+      vim.api.nvim_create_autocmd('ModeChanged', {
+        group = vim.api.nvim_create_augroup('UnlinkSnippetOnModeChange', { clear = true }),
+        pattern = { 's:n', 'i:*' },
+        desc = 'Forget the current snippet when leaving the insert mode',
+        callback = function(evt)
+          if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+            luasnip.unlink_current()
+          end
+        end,
+      })
     end,
   },
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -96,15 +96,15 @@ return {
     }
 
     -- Keymaps
-    vim.keymap.set('n', '<Leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>')
-    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>')
-    vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>')
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>')
+    vim.keymap.set('n', '<Leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = 'Line [d]iagnostic info' })
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = 'Previous [d]iagnostic' })
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = 'Next [d]iagnostic' })
+    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', { desc = 'Go to [d]efinition' })
+    vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { desc = '[a]ction' })
+    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', { desc = 'Go to [i]mplementation' })
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', { desc = 'Go to [r]eference(s)' })
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-    vim.keymap.set('n', '<Leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+    vim.keymap.set('n', '<Leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', { desc = '[r]ename' })
 
     -- Commands
     vim.api.nvim_create_user_command('Format', function()
