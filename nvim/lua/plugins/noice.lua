b9a5ccd403875b78786b74d2360c8b00e407181b
Jack Sellars <jsellars@enee.com.au>
2023-10-26 13:10:59 +1030

    refactor

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,40 +5,66 @@ return {
     'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
   },
-  opts = {
-    routes = {
-      {
-        filter = {
-          event = 'msg_show',
-          find = '%d+L, %d+B',
+  opts = function()
+    return {
+      routes = {
+        {
+          filter = {
+            event = 'notify',
+            find = 'No information available',
+          },
+          opts = { skip = true },
+          view = 'mini',
         },
-        view = 'mini',
       },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-    },
-    lsp = {
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true,
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
       },
-      hover = {
-        enabled = false,
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
+        },
       },
-      signature = {
-        enabled = false
-      }
-    },
-  },
-  -- stylua: ignore
+    }
+  end,
   keys = {
-    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    {
+      '<leader>snl',
+      function()
+        require('noice').cmd 'last'
+      end,
+      desc = 'Noice Last Message',
+    },
+    {
+      '<leader>snh',
+      function()
+        require('noice').cmd 'history'
+      end,
+      desc = 'Noice History',
+    },
+    {
+      '<leader>sna',
+      function()
+        require('noice').cmd 'all'
+      end,
+      desc = 'Noice All',
+    },
+    {
+      '<leader>snd',
+      function()
+        require('noice').cmd 'dismiss'
+      end,
+      desc = 'Dismiss All',
+    },
   },
 }
--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -29,16 +29,4 @@ return {
       end
     end,
   },
-  {
-    'folke/noice.nvim',
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-    end,
-  },
 }
