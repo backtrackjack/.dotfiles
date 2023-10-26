4b58fe7beda88c23f1c68844501b336bb2ba84e5
Jack Sellars <jsellars@enee.com.au>
2023-10-26 12:42:25 +1030

    refactoring

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,10 +3,26 @@ return {
   cmd = { 'TroubleToggle', 'Trouble' },
   opts = { use_diagnostic_signs = true },
   keys = {
-    { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnosti[x] (Trouble)' },
-    { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnosti[X] (Trouble)' },
-    { '<leader>xL', '<cmd>TroubleToggle loclist<cr>', desc = '[L]ocation List (Trouble)' },
-    { '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', desc = '[Q]uickfix List (Trouble)' },
+    {
+      '<leader>xx',
+      '<cmd>TroubleToggle document_diagnostics<cr>',
+      desc = 'Document Diagnosti[x] (Trouble)'
+    },
+    {
+      '<leader>xX',
+      '<cmd>TroubleToggle workspace_diagnostics<cr>',
+      desc = 'Workspace Diagnosti[X] (Trouble)'
+    },
+    {
+      '<leader>xL',
+      '<cmd>TroubleToggle loclist<cr>',
+      desc = '[L]ocation List (Trouble)'
+    },
+    {
+      '<leader>xQ',
+      '<cmd>TroubleToggle quickfix<cr>',
+      desc = '[Q]uickfix List (Trouble)'
+    },
     {
       '[q',
       function()
