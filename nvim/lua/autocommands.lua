8d7985d142ad98b045dff708a4f97d9728c0f1f4
Jack Sellars <jsellars@enee.com.au>
2023-11-09 07:55:29 +1030

    moving around

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -2,27 +2,6 @@ local function augroup(name)
   return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
--- show vim tip on startup
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = augroup 'vimtip',
-  callback = function()
-    local job = require 'plenary.job'
-    job
-      :new({
-        command = 'curl',
-        args = { 'https://vtip.43z.one' },
-        on_exit = function(j, exit_code)
-          local res = table.concat(j:result())
-          if exit_code ~= 0 then
-            res = 'Error fetching tip: ' .. res
-          end
-          vim.notify(res, 2, { title = 'Tip!' })
-        end,
-      })
-      :start()
-  end,
-})
-
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup 'highlight_yank',
--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -28,5 +28,22 @@ return {
         end)
       end
     end,
+    config = function()
+      -- show tip on startup
+      local job = require 'plenary.job'
+      job
+        :new({
+          command = 'curl',
+          args = { 'https://vtip.43z.one' },
+          on_exit = function(j, exit_code)
+            local res = table.concat(j:result())
+            if exit_code ~= 0 then
+              res = 'Error fetching tip: ' .. res
+            end
+            require 'notify'(res, 2, { title = 'Tip!' })
+          end,
+        })
+        :start()
+    end,
   },
 }
