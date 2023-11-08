8a24ab088ed0b66bbbe216477afa75989a7bfa43
Jack Sellars <jsellars@enee.com.au>
2023-11-08 21:25:49 +1030

    navigate qflist with ]c instead of ]q

--- nvim/lua/plugins/mini/bracketed.lua
+++ nvim/lua/plugins/mini/bracketed.lua
@@ -1,8 +1,12 @@
 return {
   'echasnovski/mini.bracketed',
   opts = {
+    -- disable
     undo = { suffix = '' },
     treesitter = { suffix = '' },
     jump = { suffix = '' },
+    comment = { suffix = '' },
+    -- customize
+    quickfix = { suffix = 'c' },
   },
 }
