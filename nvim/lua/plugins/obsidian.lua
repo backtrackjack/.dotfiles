9554f63b0e4eb61a684a5ec6c629553e9e5a89c7
Jack Sellars <jsellars@enee.com.au>
2023-11-16 10:46:14 +1030

    whack or is it wack

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,6 +1,5 @@
 return {
   'epwalsh/obsidian.nvim',
-  version = '*',
   cmd = { 'ObsidianQuickSwitch' }, -- used in alpha dashboard
   event = {
     'BufReadPre ' .. vim.fn.expand '~' .. '/.dotfiles/notes/vaults/main/**.md',
@@ -10,7 +9,6 @@ return {
   keys = {
     { '<leader>nf', '<cmd>ObsidianQuickSwitch<cr>', desc = '[f]ind note' },
     { '<leader>ns', '<cmd>ObsidianSearch<cr>', desc = '[s]earch' },
-    { '<leader>no', '<cmd>ObsidianOpen<cr>', desc = '[o]pen in obsidian' },
     { '<leader>nn', ':ObsidianNew ', desc = '[n]ew' },
     { '<leader>nd', '<cmd>ObsidianToday<cr>', desc = '[d]aily' },
   },
@@ -51,48 +49,60 @@ return {
       vim.fn.jobstart { 'open', url } -- macos
     end,
     open_app_foreground = true,
-    -- mappings = {
-    --   -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-    --   ['gf'] = {
-    --     action = function()
-    --       return require('obsidian').util.gf_passthrough()
-    --     end,
-    --     opts = { noremap = false, expr = true, buffer = true, desc = '[f]ollow link / go to [f]ile' },
-    --   },
-    --   -- Toggle check-boxes.
-    --   ['<leader>ch'] = {
-    --     action = function()
-    --       return require('obsidian').util.toggle_checkbox()
-    --     end,
-    --     opts = { buffer = true, desc = 'toggle [ch]eckbox' },
-    --   },
-    -- }, -- Optional, configure additional syntax highlighting / extmarks.
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ['gf'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true, desc = '[f]ollow link / go to [f]ile' },
+      },
+      -- Toggle check-boxes.
+      ['<leader>ch'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true, desc = 'toggle [ch]eckbox' },
+      },
+      ['<leader>nb'] = {
+        action = function()
+          return vim.cmd 'ObsidianBacklinks'
+        end,
+        opts = { buffer = true, desc = '[b]acklinks' },
+      },
+      ['<leader>no'] = {
+        action = function()
+          return vim.cmd 'ObsidianOpen'
+        end,
+        opts = { buffer = true, desc = '[o]pen in obsidian' },
+      },
+    }, -- Optional, configure additional syntax highlighting / extmarks.
   },
-  config = function(_, opts)
-    local obs = require 'obsidian'
-    obs.setup(opts)
-
-    local function map(mode, lhs, rhs, options)
-      options = options or {}
-      options.silent = options.silent ~= false
-      options.noremap = options.noremap ~= false
-      options.expr = options.expr ~= true
-      options.buffer = options.buffer ~= true
-      vim.keymap.set(mode, lhs, rhs, options)
-    end
-
-    vim.api.nvim_create_autocmd('BufWinEnter', {
-      group = vim.api.nvim_create_augroup('Obsidian_test', { clear = true }),
-      pattern = vim.fn.expand '~' .. '/.dotfiles/notes/vaults/main/**.md',
-      callback = function()
-        map('n', '<leader>nb', '<cmd>ObsidianBacklinks<cr>', { desc = '[b]acklinks' })
-        map('n', 'gf', function()
-          require('obsidian').util.gf_passthrough()
-        end, { desc = '[f]ollow link / go to [f]ile' })
-        map('n', '<leader>ch', function()
-          require('obsidian').util.toggle_checkbox()
-        end, { desc = 'toggle [ch]eckbox' })
-      end,
-    })
-  end,
+  -- config = function(_, opts)
+  --   local obs = require 'obsidian'
+  --   obs.setup(opts)
+  --
+  --   local function map(mode, lhs, rhs, options)
+  --     options = options or {}
+  --     options.silent = options.silent ~= false
+  --     options.noremap = options.noremap ~= false
+  --     options.expr = options.expr ~= true
+  --     options.buffer = options.buffer ~= true
+  --     vim.keymap.set(mode, lhs, rhs, options)
+  --   end
+  --
+  --   vim.api.nvim_create_autocmd('BufWinEnter', {
+  --     group = vim.api.nvim_create_augroup('Obsidian_test', { clear = true }),
+  --     pattern = vim.fn.expand '~' .. '/.dotfiles/notes/vaults/main/**.md',
+  --     callback = function()
+  --       map('n', '<leader>nb', '<cmd>ObsidianBacklinks<cr>', { desc = '[b]acklinks' })
+  --       map('n', 'gf', function()
+  --         require('obsidian').util.gf_passthrough()
+  --       end, { desc = '[f]ollow link / go to [f]ile' })
+  --       map('n', '<leader>ch', function()
+  --         require('obsidian').util.toggle_checkbox()
+  --       end, { desc = 'toggle [ch]eckbox' })
+  --     end,
+  --   })
+  -- end,
 }
