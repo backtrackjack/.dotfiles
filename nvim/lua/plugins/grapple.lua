7a7a179dc7c55964536798e9443aa07bae986c9a
Jack Sellars <jsellars@enee.com.au>
2024-03-04 10:29:44 +1030

    updates. grapple stuff

--- nvim/lua/plugins/grapple.lua
+++ nvim/lua/plugins/grapple.lua
@@ -2,9 +2,9 @@ return {
   'cbochs/grapple.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<c-e>', '<cmd>GrapplePopup tags<cr>' },
-    { '<leader>+', '<cmd>GrappleTag<cr>', desc = '[+] grapple list' },
-    { 'L', '<cmd>GrappleCycle forward<cr>', desc = 'Cycle grapple forward' },
-    { 'H', '<cmd>GrappleCycle backward<cr>', desc = 'Cycle grapple backward' },
+    { '<c-e>', '<cmd>Grapple open_tags<cr>' },
+    { '<leader>+', '<cmd>Grapple tag<cr>', desc = '[+] grapple list' },
+    { 'L', '<cmd>Grapple cycle forward<cr>', desc = 'Cycle grapple forward' },
+    { 'H', '<cmd>Grapple cycle backward<cr>', desc = 'Cycle grapple backward' },
   },
 }
